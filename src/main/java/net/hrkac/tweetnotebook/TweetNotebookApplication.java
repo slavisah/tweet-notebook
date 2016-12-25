67070c5c74da333947e426de88082f561174aa56
slavisah <shrkac@gmail.com>
2016-12-26 00:33:36 +0100

    first commit

---
+++ src/main/java/net/hrkac/tweetnotebook/TweetNotebookApplication.java
@@ -0,0 +1,12 @@
+package net.hrkac.tweetnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetNotebookApplication.class, args);
+	}
+}
---
+++ src/test/java/net/hrkac/tweetnotebook/TweetNotebookApplicationTests.java
@@ -0,0 +1,16 @@
+package net.hrkac.tweetnotebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweetNotebookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
