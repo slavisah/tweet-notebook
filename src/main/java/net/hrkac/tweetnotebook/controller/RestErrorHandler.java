adf0e420f93c778e435c35d3a134944b63f64aa4
slavisah <shrkac@gmail.com>
2017-01-02 16:05:08 +0100

    design and development

--- src/main/java/net/hrkac/tweetnotebook/controller/RestErrorHandler.java
+++ src/main/java/net/hrkac/tweetnotebook/controller/RestErrorHandler.java
@@ -57,8 +57,6 @@ public class RestErrorHandler {
         Locale currentLocale =  LocaleContextHolder.getLocale();
         String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
 
-        //If the message was not found, return the most accurate field error code instead.
-        //You can remove this check if you prefer to get the default error message.
         if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
             String[] fieldErrorCodes = fieldError.getCodes();
             localizedErrorMessage = fieldErrorCodes[0];
