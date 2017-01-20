6f03d390cb581ca9c9a2c0423201a3d208f31a48
slavisah <shrkac@gmail.com>
2017-01-20 16:14:57 +0100

    design and development

--- src/test/java/net/hrkac/tweetnotebook/service/NoteServiceTest.java
+++ src/test/java/net/hrkac/tweetnotebook/service/NoteServiceTest.java
@@ -13,11 +13,21 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import net.hrkac.tweetnotebook.dao.NoteDao;
+import net.hrkac.tweetnotebook.dto.NoteDTO;
+import net.hrkac.tweetnotebook.exception.NoteNotFoundException;
 import net.hrkac.tweetnotebook.model.Note;
+import net.hrkac.tweetnotebook.model.TestNoteBuilder;
 
 public class NoteServiceTest {
+    
+    private static final Long ID = 1L;
+    private static final String TITLE = "title";
+    private static final String TITLE_UPDATED = "updatedTitle";
+    private static final String TEXT = "text";
+    private static final String TEXT_UPDATED = "updatedText";
     
     private NoteService noteService;
     private NoteDao noteDaoMock;
@@ -26,6 +36,23 @@ public class NoteServiceTest {
     public void setup() {
         noteDaoMock = mock(NoteDao.class);
         noteService = new NoteService(noteDaoMock);
+    }
+    
+    @Test
+    public void add_NewNote_ShouldSaveNote() {
+        NoteDTO dto = NoteDTO.getBuilder().title(TITLE).text(TEXT).build();
+
+        noteService.add(dto);
+
+        ArgumentCaptor<Note> noteArgument = ArgumentCaptor.forClass(Note.class);
+        verify(noteDaoMock, times(1)).save(noteArgument.capture());
+        verifyNoMoreInteractions(noteDaoMock);
+
+        Note model = noteArgument.getValue();
+
+        assertNull(model.getId());        
+        assertThat(model.getTitle(), is(dto.getTitle()));
+        assertThat(model.getText(), is(dto.getText()));
     }
 
     @Test
@@ -39,6 +66,85 @@ public class NoteServiceTest {
         verifyNoMoreInteractions(noteDaoMock);
 
         assertThat(actual, is(models));
+    }
+    
+    @Test
+    public void findById_NoteFound_ShouldReturnFoundNote() throws NoteNotFoundException {
+        Note model = new TestNoteBuilder().id(ID).title(TITLE).text(TEXT).build();
+
+        when(noteDaoMock.findOne(ID)).thenReturn(model);
+
+        Note actual = noteService.findById(ID);
+
+        verify(noteDaoMock, times(1)).findOne(ID);
+        verifyNoMoreInteractions(noteDaoMock);
+
+        assertThat(actual, is(model));
+    }
+
+    @Test(expected = NoteNotFoundException.class)
+    public void findById_TodoEntryNotFound_ShouldThrowException() throws NoteNotFoundException {
+        when(noteDaoMock.findOne(ID)).thenReturn(null);
+
+        noteService.findById(ID);
+
+        verify(noteDaoMock, times(1)).findOne(ID);
+        verifyNoMoreInteractions(noteDaoMock);
+    }
+    
+    @Test
+    public void update_NoteFound_ShouldUpdateNote() throws NoteNotFoundException {
+        NoteDTO dto = NoteDTO.getBuilder().id(ID).title(TITLE_UPDATED).text(TEXT_UPDATED).build();
+
+        Note model = new TestNoteBuilder().id(ID).title(TITLE).text(TEXT).build();
+
+        when(noteDaoMock.findOne(dto.getId())).thenReturn(model);
+
+        noteService.update(dto);
+
+        verify(noteDaoMock, times(1)).findOne(dto.getId());
+        verifyNoMoreInteractions(noteDaoMock);
+
+        assertThat(model.getId(), is(dto.getId()));
+        assertThat(model.getTitle(), is(dto.getTitle()));
+        assertThat(model.getText(), is(dto.getText()));
+    }
+
+    @Test(expected = NoteNotFoundException.class)
+    public void update_NoteNotFound_ShouldThrowException() throws NoteNotFoundException {
+        NoteDTO dto = NoteDTO.getBuilder().id(ID).title(TITLE_UPDATED).text(TEXT).build();
+
+        when(noteDaoMock.findOne(dto.getId())).thenReturn(null);
+
+        noteService.update(dto);
+
+        verify(noteDaoMock, times(1)).findOne(dto.getId());
+        verifyNoMoreInteractions(noteDaoMock);
+    }
+    
+    @Test
+    public void deleteById_NoteFound_ShouldDeleteNoteEntryAndReturnIt() throws NoteNotFoundException {
+        Note model = new TestNoteBuilder().id(ID).title(TITLE).text(TEXT).build();
+
+        when(noteDaoMock.findOne(ID)).thenReturn(model);
+
+        Note actual = noteService.deleteById(ID);
+
+        verify(noteDaoMock, times(1)).findOne(ID);
+        verify(noteDaoMock, times(1)).delete(model);
+        verifyNoMoreInteractions(noteDaoMock);
+
+        assertThat(actual, is(model));
+    }
+
+    @Test(expected = NoteNotFoundException.class)
+    public void deleteById_TodoEntryNotFound_ShouldThrowException() throws NoteNotFoundException {
+        when(noteDaoMock.findOne(ID)).thenReturn(null);
+
+        noteService.deleteById(ID);
+
+        verify(noteDaoMock, times(1)).findOne(ID);
+        verifyNoMoreInteractions(noteDaoMock);
     }
 
 }
