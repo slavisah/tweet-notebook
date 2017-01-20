3988796ca0434ddc672d007e8d5e4a053399bfb2
slavisah <shrkac@gmail.com>
2017-01-20 16:44:59 +0100

    design and development

--- src/main/java/net/hrkac/tweetnotebook/TweetNotebookApplication.java
+++ src/main/java/net/hrkac/tweetnotebook/TweetNotebookApplication.java
@@ -3,12 +3,8 @@ package net.hrkac.tweetnotebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.test.context.ContextConfiguration;
-
-import net.hrkac.tweetnotebook.config.ExampleApplicationContext;
 
 @SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
-@ContextConfiguration(classes = {ExampleApplicationContext.class})
 public class TweetNotebookApplication {
 
 	public static void main(String[] args) {
