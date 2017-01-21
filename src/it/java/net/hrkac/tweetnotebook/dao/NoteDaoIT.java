59f306f82d08bf7938cba18e72b31fda228959e4
slavisah <shrkac@gmail.com>
2017-01-21 22:19:07 +0100

    design and development

--- src/it/java/net/hrkac/tweetnotebook/dao/NoteDaoIT.java
+++ src/it/java/net/hrkac/tweetnotebook/dao/NoteDaoIT.java
@@ -3,6 +3,8 @@ package net.hrkac.tweetnotebook.dao;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import net.hrkac.tweetnotebook.config.ExampleApplicationContext;
+import net.hrkac.tweetnotebook.model.Note;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +21,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
-
-import net.hrkac.tweetnotebook.config.ExampleApplicationContext;
-import net.hrkac.tweetnotebook.model.Note;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {ExampleApplicationContext.class})
@@ -36,13 +36,36 @@ public class NoteDaoIT {
     
     @Autowired
     private NoteDao noteDao;
+    
+    @Test
+    @ExpectedDatabase(value = "noteData-add-expected.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void add_NewNote_ShouldAddNoteAndReturnAddedEntry() {
+        Note model = Note.getBuilder("Example 3").text("Lorem ipsum").build();
+        noteDao.save(model);
+    }
 
     @Test
     @ExpectedDatabase("noteData.xml")
-    public void findById_NoteFound_ShouldReturnOneNoteWithId() {
+    public void findOne_NoteFound_ShouldReturnOneNoteWithId() {
         Note found = noteDao.findOne(1L);
         assertNotNull(found);
-        assertThat(found.getId(), equalTo(1L));;
+        assertThat(found.getId(), equalTo(1L));
+        assertThat(found.getText(), equalTo("Lorem ipsum"));
+    }
+    
+    @Test
+    @ExpectedDatabase(value = "noteData-update-expected.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void update_NoteFound_ShouldUpdateNoteEntry() {
+        Note model = noteDao.findOne(1L);
+        model.update("Text", "Title");
+        noteDao.saveAndFlush(model);
+    }
+    
+    @Test
+    @ExpectedDatabase("noteData-delete-expected.xml")
+    public void deleteById_NoteFound_ShouldDeleteNoteEntry() {
+        Note deleted = noteDao.findOne(1L);
+        noteDao.delete(deleted);
     }
 
 }
